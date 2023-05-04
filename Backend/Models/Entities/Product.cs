85b0506a245d2fe60faa2bad003cbfef1d86dbf3
HubSor <44928255+HubSor@users.noreply.github.com>
2023-05-04 14:19:34 +0200

    UPS-2 Dodanie szkieletów aplikacji.

---
+++ Backend/Models/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Models.Entities
+{
+    public class Product : Entity
+    {
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ Backend/UPS/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UPS;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthorization();
+builder.Services.AddDbContext<UPSContext>(options =>
+   options.UseNpgsql(builder.Configuration.GetConnectionString("UPS_Connection")));
+
+var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<UPSContext>();
+        var created = context.Database.EnsureCreated();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred creating the DB.");
+    }
+}
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.Run();
