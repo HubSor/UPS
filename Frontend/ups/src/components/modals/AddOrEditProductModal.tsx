c5132b0829e6c447c13e5d5137d40ceda8515d83
hubsor <hubisoroka2@gmail.com>
2023-10-31 15:56:10 +0100

    UPS-28 Auto uppercase na kodach.

--- Frontend/ups/src/components/modals/AddOrEditProductModal.tsx
+++ Frontend/ups/src/components/modals/AddOrEditProductModal.tsx
@@ -7,6 +7,7 @@ import { ApiResponse } from "../../api/ApiResponses";
 import { toastDefaultError, toastInfo } from "../../helpers/ToastHelpers";
 import { EditProductRequest } from "../../api/ApiRequests";
 import { boolean, number, object, string } from "yup";
+import { useEffect } from "react";
 
 type AddOrEditProductModalProps = {
     onSuccess: () => void
@@ -75,31 +76,38 @@ export function AddOrEditProductModal({ onSuccess, close, editedProduct }: AddOr
                     Api.AddProduct(values).then(res => handleApiResponse(res, false))
             }}
         >
-            {({ isSubmitting }) => <Form>
-                <Modal.Header className="darkblue">
-                    <Modal.Title>
-                        {editMode ? "Edytuj produkt" : "Dodaj produkt"}
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <TypeInputGroup name="name" label="Nazwa" type="text"/>
-                    <ValidationMessage fieldName="id" />
-                    <TypeInputGroup name="code" label="Kod" type="text"/>
-                    <TypeInputGroup name="basePrice" label="Podstawowa cena" type="number"/>
-                    {editMode && <AsInputGroup name="status" label="Status" as="select" options={ProductOptions}/>}
-                    <CheckboxInputGroup name="anonymousSaleAllowed" label="Anonimowa sprzedaż"/>
-                    <AsInputGroup name="description" label="Opis" as="textarea"/>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button type="submit" disabled={isSubmitting}>
-                        Zapisz
-                    </Button>
-                    &nbsp;
-                    <Button type="button" variant="danger" onClick={close}>
-                        Anuluj
-                    </Button>
-                </Modal.Footer>
-            </Form>}
+            {function FormInner({ isSubmitting, values, setFieldValue }) {
+                useEffect(() => {
+                    if (values.code.toUpperCase() !== values.code)
+                        setFieldValue("code", values.code.toUpperCase())
+                }, [values.code, setFieldValue]);
+
+                return <Form>
+                    <Modal.Header className="darkblue">
+                        <Modal.Title>
+                            {editMode ? "Edytuj produkt" : "Dodaj produkt"}
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <TypeInputGroup name="name" label="Nazwa" type="text"/>
+                        <ValidationMessage fieldName="id" />
+                        <TypeInputGroup name="code" label="Kod" type="text"/>
+                        <TypeInputGroup name="basePrice" label="Podstawowa cena" type="number"/>
+                        {editMode && <AsInputGroup name="status" label="Status" as="select" options={ProductOptions}/>}
+                        <CheckboxInputGroup name="anonymousSaleAllowed" label="Anonimowa sprzedaż"/>
+                        <AsInputGroup name="description" label="Opis" as="textarea"/>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button type="submit" disabled={isSubmitting}>
+                            Zapisz
+                        </Button>
+                        &nbsp;
+                        <Button type="button" variant="danger" onClick={close}>
+                            Anuluj
+                        </Button>
+                    </Modal.Footer>
+                </Form>}
+            }
         </Formik>
     </Modal>
 }
--- Frontend/ups/src/components/modals/AddOrEditSubProductModal.tsx
+++ Frontend/ups/src/components/modals/AddOrEditSubProductModal.tsx
@@ -7,6 +7,7 @@ import { ApiResponse } from "../../api/ApiResponses";
 import { toastDefaultError, toastInfo } from "../../helpers/ToastHelpers";
 import { EditSubProductRequest } from "../../api/ApiRequests";
 import { number, object, string } from "yup";
+import { useEffect } from "react";
 
 type AddOrEditSubProductModalProps = {
     onSuccess: () => void
@@ -64,29 +65,36 @@ export function AddOrEditSubProductModal({ onSuccess, close, editedSubProduct }:
                     Api.AddSubProduct(v).then(res => handleApiResponse(res, false))
             }}
         >
-            {({ isSubmitting }) => <Form>
-                <Modal.Header className="darkblue">
-                    <Modal.Title>
-                        {editMode ? "Edytuj podprodukt" : "Dodaj podprodukt"}
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <TypeInputGroup name="name" label="Nazwa" type="text"/>
-                    <ValidationMessage fieldName="id" />
-                    <TypeInputGroup name="code" label="Kod" type="text"/>
-                    <TypeInputGroup name="basePrice" label="Podstawowa cena" type="number"/>
-                    <AsInputGroup name="description" label="Opis" as="textarea"/>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button type="submit" disabled={isSubmitting}>
-                        Zapisz
-                    </Button>
-                    &nbsp;
-                    <Button type="button" variant="danger" onClick={close}>
-                        Anuluj
-                    </Button>
-                </Modal.Footer>
-            </Form>}
+            {function FormInner({ isSubmitting, values, setFieldValue }) { 
+                useEffect(() => {
+                    if (values.code.toUpperCase() !== values.code)
+                        setFieldValue("code", values.code.toUpperCase())
+                }, [values.code, setFieldValue]);
+                
+                return <Form>
+                    <Modal.Header className="darkblue">
+                        <Modal.Title>
+                            {editMode ? "Edytuj podprodukt" : "Dodaj podprodukt"}
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <TypeInputGroup name="name" label="Nazwa" type="text"/>
+                        <ValidationMessage fieldName="id" />
+                        <TypeInputGroup name="code" label="Kod" type="text"/>
+                        <TypeInputGroup name="basePrice" label="Podstawowa cena" type="number"/>
+                        <AsInputGroup name="description" label="Opis" as="textarea"/>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button type="submit" disabled={isSubmitting}>
+                            Zapisz
+                        </Button>
+                        &nbsp;
+                        <Button type="button" variant="danger" onClick={close}>
+                            Anuluj
+                        </Button>
+                    </Modal.Footer>
+                </Form>}
+            }
         </Formik>
     </Modal>
 }
