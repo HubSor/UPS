c3780d1a89260b27ffd9b5dc1876423f68526e51
hubsor <hubisoroka2@gmail.com>
2023-11-01 15:13:21 +0100

    UPS-28 Endpointy edycji i usuwania.

--- Backend/Messages/Parameters/ParametersOrders.cs
+++ Backend/Messages/Parameters/ParametersOrders.cs
@@ -4,7 +4,7 @@ using Models.Entities;
 namespace Messages.Parameters;
 
 public record AddParameterOrder(string Name, bool Required, ParameterTypeEnum Type, int? ProductId, int? SubProductId, ICollection<OptionDto>? Options);
-public record EditParameterOrder(int ParameterId, string Name, bool Required, ParameterTypeEnum Type);
+public record EditParameterOrder(int ParameterId, string Name, bool Required, ParameterTypeEnum Type, ICollection<OptionDto>? Options);
 public record DeleteParameterOrder(int ParameterId);
 public record AddOptionOrder(int ParameterId, string Value);
 public record DeleteOptionOrder(int OptionId);
--- Backend/UPS/Controllers/ParametersController.cs
+++ Backend/UPS/Controllers/ParametersController.cs
@@ -24,10 +24,34 @@ namespace UPS.Controllers
 
 		[HttpPost]
 		[AuthorizeRoles(RoleEnum.ProductManager, RoleEnum.Administrator)]
+		[Route("edit")]
+		public async Task<IActionResult> Edit([FromBody] EditParameterOrder order)
+		{
+			return await RespondAsync<EditParameterOrder, EditParameterResponse>(order);
+		}
+
+		[HttpPost]
+		[AuthorizeRoles(RoleEnum.ProductManager, RoleEnum.Administrator)]
+		[Route("delete")]
+		public async Task<IActionResult> Delete([FromBody] DeleteParameterOrder order)
+		{
+			return await RespondAsync<DeleteParameterOrder, DeleteParameterResponse>(order);
+		}
+
+		[HttpPost]
+		[AuthorizeRoles(RoleEnum.ProductManager, RoleEnum.Administrator)]
 		[Route("options/add")]
 		public async Task<IActionResult> AddOption([FromBody] AddOptionOrder order)
 		{
 			return await RespondAsync<AddOptionOrder, AddOptionResponse>(order);
+		}
+
+		[HttpPost]
+		[AuthorizeRoles(RoleEnum.ProductManager, RoleEnum.Administrator)]
+		[Route("options/delete")]
+		public async Task<IActionResult> DeleteOption([FromBody] DeleteOptionOrder order)
+		{
+			return await RespondAsync<DeleteOptionOrder, DeleteOptionResponse>(order);
 		}
 	}
 }
